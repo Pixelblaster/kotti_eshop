01097738f13dd0a796cb0c5e4fc0dcda745778bf
Ghiță Bizău <ghitabizau27@gmail.com>
2015-02-20 12:13:02 +0200

    Add and edit shop views.

--- kotti_eshop/views/edit.py
+++ kotti_eshop/views/edit.py
@@ -7,6 +7,7 @@ from pyramid.view import view_config
 
 from kotti_eshop import _
 from kotti_eshop.resources import CustomContent
+from kotti_eshop.resources import Shop
 
 
 class CustomContentSchema(ContentSchema):
@@ -15,6 +16,28 @@ class CustomContentSchema(ContentSchema):
     custom_attribute = colander.SchemaNode(
         colander.String(),
         title=_(u"Custom attribute"))
+
+
+class ShopSchema(ContentSchema):
+    """ Schema for Shop. """
+
+
+@view_config(name=Shop.type_info.add_view, permission='add',
+             renderer='kotti:templates/edit/node.pt')
+class ShopAddForm(AddFormView):
+    """ Form to add a new Shop. """
+
+    schema_factory = ShopSchema
+    add = Shop
+    item_type = _(u"Shop")
+
+
+@view_config(name='edit', context=Shop, permission='edit',
+             renderer='kotti:templates/edit/node.pt')
+class ShopEditForm(EditFormView):
+    """ Form to edit existing Shop objects. """
+
+    schema_factory = ShopSchema
 
 
 @view_config(name=CustomContent.type_info.add_view, permission='add',
