6b1e7f4c87200001e75401a8a36650ea52594ff0
Ghiță Bizău <ghitabizau27@gmail.com>
2015-02-27 16:19:33 +0200

    Test shop model.

--- kotti_eshop/tests/test_resources.py
+++ kotti_eshop/tests/test_resources.py
@@ -16,3 +16,19 @@ def test_model(root, db_session):
 
     with raises(TypeError):
         cc = CustomContent(doesnotexist=u'Foo')
+
+
+def test_shop_model(root, db_session):
+    from kotti_eshop.resources import Shop
+
+    shop = Shop()
+    assert shop.name is None
+
+    shop = Shop(title=u'Shop')
+    assert shop.title == u'Shop'
+
+    root['shopname'] = shop = Shop()
+    assert shop.name == 'shopname'
+
+    with raises(TypeError):
+        shop = Shop(doesnotexist=u'Foo')
