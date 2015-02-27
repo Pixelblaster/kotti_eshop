24e996212cf2c3da2c1cbeffe85931a996e4b559
Ghiță Bizău <ghitabizau27@gmail.com>
2015-02-27 12:32:31 +0200

    Test for adding a product to a shop.

--- kotti_eshop/tests/test_functional.py
+++ kotti_eshop/tests/test_functional.py
@@ -74,10 +74,37 @@ def test_edit_shop(webtest, root):
     """ Test: Edit a shop """
     from kotti_eshop.resources import Shop
 
-    root['cc'] = Shop(title=u'Shop Title')
+    root['shop'] = Shop(title=u'Shop Title')
 
-    resp = webtest.get('/cc/@@edit')
+    resp = webtest.get('/shop/@@edit')
     form = resp.forms['deform']
     assert form['title'].value == u'Shop Title'
     resp = form.submit('save').maybe_follow()
     assert u'Your changes have been saved.' in resp.body
+
+
+@mark.user('admin')
+def test_add_product(webtest, root):
+    """ Test: Add a product to shop """
+    from kotti_eshop.resources import Shop
+    root['shop'] = Shop(title=u'Shop Title')
+
+    resp = webtest.get('/shop/add_product')
+
+    # submit empty form
+    form = resp.forms['deform']
+    resp = form.submit('save')
+    assert 'There was a problem' in resp.body
+
+    # submit valid form
+    form = resp.forms['deform']
+    form['title'] = u'A product'
+    form['price'] = 20
+    form['support_days'] = 365
+    form['featured'] = True
+    form['status'] = u"Available"
+
+    resp = form.submit('save')
+    assert resp.status_code == 302
+    resp = resp.follow()
+    assert 'Item was added.' in resp.body
