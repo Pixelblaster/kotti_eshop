347ef4b74b9c976eaaec8668aef1280dde4ed136
Ghiță Bizău <ghitabizau27@gmail.com>
2015-03-02 12:46:26 +0200

    Updates.

--- kotti_eshop/populate.py
+++ kotti_eshop/populate.py
@@ -7,31 +7,31 @@ from colander import Int
 from kotti_settings.util import add_settings
 
 
-class ShopCurrency(SchemaNode):
-    name = 'Currency'
+class ShopCurrencyNode(SchemaNode):
+    name = 'shop_currency'
     title = 'Currency'
     default = 'USD'
 
 
-class ShopProductsPerPage(SchemaNode):
-    name = 'Products per page'
+class ShopProductsPerPageNode(SchemaNode):
+    name = 'shop_products_per_page'
+    title = 'Products per page'
     validator = Range(1, 1000)
     default = 9
-    title = 'Ranged Int'
 
 
-class TestSchema(MappingSchema):
-    string = StringSchemaNode(String())
-    ranged_int = RangedIntSchemaNode(Int())
+class ShopSettingsSchema(MappingSchema):
+    shop_currency = ShopCurrencyNode(String())
+    shop_products_per_page = ShopProductsPerPageNode(Int())
 
-TestSettings = {
-    'name': 'test_settings',
-    'title': "Testsettings",
-    'description': "Some description for my settings",
-    'success_message': u"Successfully saved test settings.",
-    'schema_factory': TestSchema
+ShopSettings = {
+    'name': 'shop_settings',
+    'title': "Shop settings",
+    'description': "Here you can set your shop.",
+    'success_message': u"Successfully saved shop settings.",
+    'schema_factory': ShopSettingsSchema
 }
 
 
 def populate():
-    add_settings(TestSettings)
+    add_settings(ShopSettings)
