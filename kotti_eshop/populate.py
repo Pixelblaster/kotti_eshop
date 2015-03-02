d2b693db015dab199e76c35dab0ce6f5f4b6d163
Ghiță Bizău <ghitabizau27@gmail.com>
2015-03-02 12:36:22 +0200

    Shop settings.

--- kotti_eshop/populate.py
+++ kotti_eshop/populate.py
@@ -1,44 +1,22 @@
 from colander import MappingSchema
 from colander import SchemaNode
 from colander import Range
-from colander import Set
 from colander import String
 from colander import Int
-from deform.widget import CheckboxChoiceWidget
-from kotti import get_settings
 
-from kotti_disqus import _
 from kotti_settings.util import add_settings
 
 
-TestSettings = {
-    'name': 'test_settings',
-    'title': "Testsettings",
-    'description': "Some description for my settings",
-    'success_message': u"Successfully saved test settings.",
-    'settings': [
-        {'type': 'String',
-         'name': 'testsetting_1',
-         'title': 'Test 1',
-         'description': 'a test setting',
-         'default': '', },
-        {'type': 'Integer',
-         'name': 'testsetting_2',
-         'title': 'Test 2',
-         'description': 'again a test setting',
-         'default': 23, }]}
+class ShopCurrency(SchemaNode):
+    name = 'Currency'
+    title = 'Currency'
+    default = 'USD'
 
 
-class StringSchemaNode(SchemaNode):
-    name = 'a_string'
-    title = 'hello'
-    default = 'hello world'
-
-
-class RangedIntSchemaNode(SchemaNode):
-    name = 'range_int'
-    validator = Range(0, 10)
-    default = 5
+class ShopProductsPerPage(SchemaNode):
+    name = 'Products per page'
+    validator = Range(1, 1000)
+    default = 9
     title = 'Ranged Int'
 
 
