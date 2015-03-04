9e0e1fe076e72522252a7bfffc1c373564e84a10
Ghiță Bizău <ghitabizau27@gmail.com>
2015-03-04 17:11:36 +0200

    Add to cart, add to wishlist - JS, WIP.

--- kotti_eshop/static/scripts.js
+++ kotti_eshop/static/scripts.js
@@ -1,1 +1,20 @@
 /* JS */
+$(document).ready(function(){
+	// ADD TO CART
+	// 	* class="add-to-cart"
+	// 	* product-id="product.id"
+	$(".add-to-cart").on("click", function(evt){
+		var product_id = $(this).attr("product-id");
+		alert("Added to cart. Product id: " + product_id);
+		evt.preventDefault();
+	});
+
+	// ADD TO WISHLIST
+	// 	* class="add-to-wishlist"
+	// 	* product-id="product.id"
+	$(".add-to-wishlist").on("click", function(evt){
+		var product_id = $(this).attr("product-id");
+		alert("Added to wishlist. Product id: " + product_id);
+		evt.preventDefault();
+	});
+});
