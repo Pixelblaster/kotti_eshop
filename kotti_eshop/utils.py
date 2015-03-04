22a87a57175b5727b230ff51b8eb7d9e673f6c12
Ghiță Bizău <ghitabizau27@gmail.com>
2015-03-04 12:56:24 +0200

    Updates.

--- kotti_eshop/utils.py
+++ kotti_eshop/utils.py
@@ -3,6 +3,8 @@ import ast
 
 def string_to_list(string_list):
     """ Convert string to list
+        u"[{'product_id': 5, 'product_quantity': 12}]"
+        [{'product_id': 5, 'product_quantity': 12}]
     """
     try:
         result = ast.literal_eval(string_list)
