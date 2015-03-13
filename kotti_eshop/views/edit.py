6df45051c9cfb72c093566d7d4cb122db0622846
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2015-03-13 17:44:49 +0200

    Added stuf add product form

--- kotti_eshop/views/edit.py
+++ kotti_eshop/views/edit.py
@@ -1,6 +1,27 @@
+#import colander
+from kotti.views.edit.content import ContentSchema
+from kotti.views.form import AddFormView
+from kotti_eshop.resources import ShopProduct
 from pyramid.view import view_config
+from kotti_eshop import _
+
+
+class ProductSchema(ContentSchema):
+    """
+    """
+
+
+@view_config(name="add-product", permission="view",
+             renderer="kotti:templates/edit/node.pt")
+class ProductAddForm(AddFormView):
+    schema_factory = ProductSchema
+    add = ShopProduct
+    item_type = _(u"Product")
+
 
 @view_config(name='edit-as-product', permission='view',
              renderer='kotti_eshop:templates/edit/product.pt')
 def edit_as_product(context, request):
     return {}
+
+
